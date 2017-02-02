1ab100ee1179816c293d2f49b2bede66526350f2
Fredrik Svensson <fredrik.a.svensson@ericsson.com>
2017-02-02 14:27:41 +0100

    Added config for Travis tests

--- test/test_setup.sh
+++ test/test_setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Init submodules
+git submodule init
+git submodule update
+
 # fetch calvin-base and install required packages
 git clone https://github.com/EricssonResearch/calvin-base.git
 cd calvin-base
