a78e0a7ae80e3ac45430a40f7f0dcadaffbfa66c
Fredrik Svensson <fredrik.a.svensson@ericsson.com>
2017-03-08 15:53:52 +0100

    tests: Return proper exit code in tests

--- test/test.sh
+++ test/test.sh
@@ -4,37 +4,28 @@ cp test/calvin.confLOCAL calvin.conf
 PYTHONPATH=calvin-base python calvin-base/calvin/Tools/csruntime.py --name rt1 -n 127.0.0.1 -p 5000 -c 5001 &
 RT1_PID=$!
 PYTHONPATH=calvin-base python test/verify_runtime.py http://127.0.0.1:5001
+exit_code+=$?
 rm calvin.conf
-
-if [ $? -ne 0 ]; then
-    echo "Failed to setup base runtime 1"
-    kill -9 $RT1_PID
-    exit
-fi
 
 cp test/calvin.confPROXY calvin.conf
 PYTHONPATH=calvin-base python calvin-base/calvin/Tools/csruntime.py --name rt2 -n 127.0.0.1 -p 5002 -c 5003 &
 RT2_PID=$!
 PYTHONPATH=calvin-base python test/verify_runtime.py http://127.0.0.1:5003
+exit_code+=$?
 rm calvin.conf
-
-if [ $? -ne 0 ]; then
-    echo "Failed to setup base runtime 2"
-    kill -9 $RT1_PID
-    kill -9 $RT2_PID
-    exit
-fi
 
 # Run dmce
 ../dmce/dmce-launcher -n 2
+
 # build and start calvin-constrained
 make -f platform/x86/Makefile
-
+exit_code+=$?
 ./calvin_c -n constrained -p "calvinip://127.0.0.1:5000" 2> cc_stderr.log &
 CONSTRAINED_RT_PID=$!
 
 # run test
 PYTHONPATH=calvin-base py.test -sv test/test.py
+exit_code+=$?
 
 ../dmce/dmce-summary cc_stderr.log
 # clean up
@@ -44,3 +35,5 @@ kill -9 $RT2_PID
 rm calvinconstrained.config
 cd calvin-base
 git checkout calvin/csparser/parsetab.py
+
+exit $exit_code
--- test/test_mpy.sh
+++ test/test_mpy.sh
@@ -4,34 +4,25 @@ cp test/calvin.confLOCAL calvin.conf
 PYTHONPATH=calvin-base python calvin-base/calvin/Tools/csruntime.py --name rt1 -n 127.0.0.1 -p 5000 -c 5001 &
 RT1_PID=$!
 PYTHONPATH=calvin-base python test/verify_runtime.py http://127.0.0.1:5001
+exit_code+=$?
 rm calvin.conf
-
-if [ $? -ne 0 ]; then
-    echo "Failed to setup base runtime 1"
-    kill -9 $RT1_PID
-    exit
-fi
 
 cp test/calvin.confPROXY calvin.conf
 PYTHONPATH=calvin-base python calvin-base/calvin/Tools/csruntime.py --name rt2 -n 127.0.0.1 -p 5002 -c 5003 &
 RT2_PID=$!
 PYTHONPATH=calvin-base python test/verify_runtime.py http://127.0.0.1:5003
+exit_code+=$?
 rm calvin.conf
-
-if [ $? -ne 0 ]; then
-    echo "Failed to setup base runtime 2"
-    kill -9 $RT1_PID
-    kill -9 $RT2_PID
-    exit
-fi
 
 # build and start calvin-constrained
 make -f platform/x86/Makefile_mpy
+exit_code+=$?
 ./calvin_c_mpy -n constrained -p "calvinip://127.0.0.1:5000" &
 CONSTRAINED_RT_PID=$!
 
 # run test
 PYTHONPATH=calvin-base py.test -sv test/test.py
+exit_code+=$?
 
 # clean up
 kill -9 $CONSTRAINED_RT_PID
@@ -39,3 +30,5 @@ kill -9 $RT1_PID
 kill -9 $RT2_PID
 cd calvin-base
 git checkout calvin/csparser/parsetab.py
+
+exit $exit_code
