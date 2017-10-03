e1b500cc862e8c8df5e895edfd7c566f216f7deb
Fredrik Svensson <fredrik.a.svensson@ericsson.com>
2017-10-03 15:21:53 +0200

    actor_temperature: Terminate calvinsys object references

--- actors/cc_actor_temperature.c
+++ actors/cc_actor_temperature.c
@@ -48,7 +48,8 @@ static cc_result_t cc_actor_temperature_init(cc_actor_t **actor, cc_list_t *mana
 		cc_platform_mem_free(state);
 		return CC_FAIL;
 	}
-	strncpy(state->temperature, obj_ref, strnlen(obj_ref, CC_UUID_BUFFER_SIZE));
+	strncpy(state->temperature, obj_ref, strlen(obj_ref));
+	state->temperature[strlen(obj_ref)] = '\0';
 
 	item = cc_list_get(managed_attributes, "period");
 	if (item == NULL) {
@@ -63,7 +64,8 @@ static cc_result_t cc_actor_temperature_init(cc_actor_t **actor, cc_list_t *mana
 		cc_platform_mem_free((void *)state);
 		return CC_FAIL;
 	}
-	strncpy(state->timer, obj_ref, strnlen(obj_ref, CC_UUID_BUFFER_SIZE));
+	strncpy(state->timer, obj_ref, strlen(obj_ref));
+	state->timer[strlen(obj_ref)] = '\0';
 
 	(*actor)->instance_state = (void *)state;
 
